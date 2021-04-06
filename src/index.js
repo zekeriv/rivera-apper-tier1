56a1a0d9f635df2a8bfae20ff288f2e7bcd54a4e
Ezekiel Rivera <ezekielrivera@ip-192-168-1-3.ap-southeast-1.compute.internal>
2021-04-06 19:01:13 +0800

    codepipeline test

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ app.use(morgan('common'))
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.json({message: `Welcome Sailor! You're home! test`})
+  res.json({message: `Welcome Sailor! You're home!`})
 })
 
 app.get('/health', (req, res) => {
-  res.json({message: `Our ship is healthy captain! test`})
+  res.json({message: `Our ship is healthy captain!`})
 })
 
 // specific error handler - not found
